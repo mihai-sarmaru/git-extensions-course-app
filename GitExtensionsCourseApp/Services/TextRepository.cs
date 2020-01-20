a8449dea4981b2893cced2d4ebfbb5a0a1f9cf83
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-01-20 18:57:50 +0200

    Add TextRepository implementation helper methods

---
+++ GitExtensionsCourseApp/Services/TextRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace GitExtensionsCourseApp.Services {
+    public class TextRepository {
+        private readonly string TEXT_FOLDER = "texts";
+
+        private bool GetTextsPath() {
+            return Directory.Exists(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, TEXT_FOLDER));
+        }
+
+        private void CreateTextsPath() {
+            Directory.CreateDirectory(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, TEXT_FOLDER));
+        }
+    }
+}
