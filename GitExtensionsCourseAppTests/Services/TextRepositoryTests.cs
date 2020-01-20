52dd2a7c3a393158086a021d1d3bbf58f7403724
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-01-20 19:31:23 +0200

    Add unit tests for directory private methods

--- GitExtensionsCourseAppTests/Services/TextRepositoryTests.cs
+++ GitExtensionsCourseAppTests/Services/TextRepositoryTests.cs
@@ -1,6 +1,8 @@
 ﻿using GitExtensionsCourseApp.Containers;
 using GitExtensionsCourseApp.Services;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
 using Unity;
 
 namespace GitExtensionsCourseAppTests.Services {
@@ -9,10 +11,24 @@ namespace GitExtensionsCourseAppTests.Services {
         private TextRepository _repo;
         private PrivateObject _privateRepo;
 
+        private readonly string TEXT_FOLDER_PATH = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "texts");
+
         [TestInitialize]
         public void Initialize() {
             _repo = ContainerHelper.Container.Resolve<TextRepository>();
             _privateRepo = new PrivateObject(_repo);
+            if (Directory.Exists(TEXT_FOLDER_PATH)) Directory.Delete(TEXT_FOLDER_PATH, true); 
+        }
+
+        [TestMethod]
+        public void TestGetTextsPath() {
+            Assert.IsFalse((bool)_privateRepo.Invoke("GetTextsPath"));
+        }
+
+        [TestMethod]
+        public void TestCreateTextsPath() {
+            _privateRepo.Invoke("CreateTextsPath");
+            Assert.IsTrue((bool)_privateRepo.Invoke("GetTextsPath"));
         }
     }
 }
