649ac2c24dc4e0d13592decf37c970bac8e4c7f2
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-01-20 19:06:11 +0200

    Add ContainerHelper singleton class
    Implement ITextRepository interface to service class

---
+++ GitExtensionsCourseApp/Containers/ContainerHelper.cs
@@ -0,0 +1,13 @@
+﻿using GitExtensionsCourseApp.Services;
+using Unity;
+using Unity.Lifetime;
+
+namespace GitExtensionsCourseApp.Containers {
+    public static class ContainerHelper {
+        public static IUnityContainer Container { get; private set; }
+        static ContainerHelper() {
+            Container = new UnityContainer();
+            Container.RegisterType<ITextRepository, TextRepository>(new ContainerControlledLifetimeManager());
+        }
+    }
+}
--- GitExtensionsCourseApp/Services/TextRepository.cs
+++ GitExtensionsCourseApp/Services/TextRepository.cs
@@ -1,9 +1,15 @@
-﻿using System;
+﻿using GitExtensionsCourseApp.Models;
+using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace GitExtensionsCourseApp.Services {
-    public class TextRepository {
+    public class TextRepository : ITextRepository {
         private readonly string TEXT_FOLDER = "texts";
+
+        public IEnumerable<Person> GetAllPersons() {
+            throw new NotImplementedException();
+        }
 
         private bool GetTextsPath() {
             return Directory.Exists(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, TEXT_FOLDER));
