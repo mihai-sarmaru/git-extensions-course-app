ba5440a7b44cd3318081c42a83cf1556f2d83b56
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-01-20 19:21:57 +0200

    Add TextRepositoryTests class with initialization
    Remove VS default test class

---
+++ GitExtensionsCourseAppTests/Services/TextRepositoryTests.cs
@@ -0,0 +1,18 @@
+﻿using GitExtensionsCourseApp.Containers;
+using GitExtensionsCourseApp.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Unity;
+
+namespace GitExtensionsCourseAppTests.Services {
+    [TestClass]
+    public class TextRepositoryTests {
+        private TextRepository _repo;
+        private PrivateObject _privateRepo;
+
+        [TestInitialize]
+        public void Initialize() {
+            _repo = ContainerHelper.Container.Resolve<TextRepository>();
+            _privateRepo = new PrivateObject(_repo);
+        }
+    }
+}
