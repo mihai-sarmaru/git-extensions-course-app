9026ee5d64a08bc651fa7b6227a038c7fb3e9688
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-01-20 18:34:14 +0200

    Add unit test project to solution

---
+++ GitExtensionsCourseAppTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GitExtensionsCourseAppTests {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+}
