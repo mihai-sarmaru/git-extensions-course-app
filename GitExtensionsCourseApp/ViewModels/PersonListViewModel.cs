d881534888c5c1b50b258c17bdecddf55f27d078
Mihai Sarmaru <mihai.sarmaru@napa.fi>
2020-01-21 14:26:15 +0200

    Add RefreshCommand to button and refresh logic

--- GitExtensionsCourseApp/ViewModels/PersonListViewModel.cs
+++ GitExtensionsCourseApp/ViewModels/PersonListViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
 using GitExtensionsCourseApp.Models;
 using GitExtensionsCourseApp.Services;
 using PropertyChanged;
@@ -8,9 +10,15 @@ namespace GitExtensionsCourseApp.ViewModels {
     public class PersonListViewModel {
         private ITextRepository _repo;
         public ObservableCollection<Person> PersonList { get; private set; }
+        public ICommand RefreshCommand { get; private set; }
 
         public PersonListViewModel(ITextRepository repo) {
             _repo = repo;
+            PersonList = new ObservableCollection<Person>(_repo.GetAllPersons());
+            RefreshCommand = new RelayCommand(OnRefresh);
+        }
+
+        private void OnRefresh() {
             PersonList = new ObservableCollection<Person>(_repo.GetAllPersons());
         }
     }
