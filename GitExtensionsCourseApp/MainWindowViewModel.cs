6e31aa5148169ad9d88f93811b724c174cd932ea
Mihai Sarmaru <mihai.sarmaru@napa.fi>
2020-01-21 14:55:25 +0200

    Add secondary content control binded to ActionsView

--- GitExtensionsCourseApp/MainWindowViewModel.cs
+++ GitExtensionsCourseApp/MainWindowViewModel.cs
@@ -5,9 +5,11 @@ using Unity;
 namespace GitExtensionsCourseApp {
     public class MainWindowViewModel {
         public object List { get; private set; }
+        public object Actions { get; private set; }
 
         public MainWindowViewModel() {
             List = ContainerHelper.Container.Resolve<PersonListViewModel>();
+            Actions = ContainerHelper.Container.Resolve<ActionsViewModel>();
         }
     }
 }
