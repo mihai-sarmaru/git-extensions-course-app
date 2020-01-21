a871a5719c4b6446e723881fafc6a4f442534da4
Mihai Sarmaru <mihai.sarmaru@napa.fi>
2020-01-21 14:54:50 +0200

    Add ActionsViewModel to project with simple constructor

---
+++ GitExtensionsCourseApp/ViewModels/ActionsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GitExtensionsCourseApp.Services;
+
+namespace GitExtensionsCourseApp.ViewModels {
+    public class ActionsViewModel {
+        private ITextRepository _repo;
+
+        public ActionsViewModel(ITextRepository repo) {
+            _repo = repo;
+        }
+    }
+}
