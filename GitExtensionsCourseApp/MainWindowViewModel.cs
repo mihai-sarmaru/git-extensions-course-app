98e94d41b82a6f48228982259373586d27e6aa84
Mihai Sarmaru <mihai.sarmaru@napa.fi>
2020-01-21 14:19:00 +0200

    Add MainWindowViewModel which exposes List property via Container

---
+++ GitExtensionsCourseApp/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using GitExtensionsCourseApp.Containers;
+using GitExtensionsCourseApp.ViewModels;
+using Unity;
+
+namespace GitExtensionsCourseApp {
+    public class MainWindowViewModel {
+        public object List { get; private set; }
+
+        public MainWindowViewModel() {
+            List = ContainerHelper.Container.Resolve<PersonListViewModel>();
+        }
+    }
+}
