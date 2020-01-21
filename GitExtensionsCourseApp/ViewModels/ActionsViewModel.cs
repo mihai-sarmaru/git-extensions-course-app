7665b9f1c5c213982734680436b570c06e804be1
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-01-21 17:47:13 +0200

    Add CalculateCommand implementation and needed properties for bindings

--- GitExtensionsCourseApp/ViewModels/ActionsViewModel.cs
+++ GitExtensionsCourseApp/ViewModels/ActionsViewModel.cs
@@ -1,16 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
 using GitExtensionsCourseApp.Services;
+using PropertyChanged;
 
 namespace GitExtensionsCourseApp.ViewModels {
+    [AddINotifyPropertyChangedInterface]
     public class ActionsViewModel {
         private ITextRepository _repo;
 
+        public double Average { get; set; }
+        public bool IsCalculated { get; set; }
+        public ICommand CalculateCommand { get; private set; }
+
         public ActionsViewModel(ITextRepository repo) {
             _repo = repo;
+            IsCalculated = false;
+            CalculateCommand = new RelayCommand(OnCalculate);
+        }
+
+        private void OnCalculate() {
+            Average = _repo.CalculateAverageAge();
+            IsCalculated = true;
         }
     }
 }
