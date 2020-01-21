fff6c9521aab036c88f4e66b944601f16e783e06
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-01-21 17:39:23 +0200

    Add BoolToVisibilityConverter with extra property

---
+++ GitExtensionsCourseApp/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace GitExtensionsCourseApp.Converters {
+    public class BoolToVisibilityConverter : IValueConverter {
+
+        public bool Reverse { get; set; } = false;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            return ConvertVisibility(value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            return ConvertVisibility(value);
+        }
+
+        private Visibility ConvertVisibility(object value) {
+            Visibility visibility = (bool)value ? Visibility.Visible : Visibility.Collapsed;
+            return !Reverse ? visibility : Visibility.Collapsed;
+        }
+    }
+}
