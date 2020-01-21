7a517112736c50e244eab5649506f1170077189f
Mihai Sarmaru <mihai.sarmaru@napa.fi>
2020-01-21 14:17:33 +0200

    Attach PersonListViewModel to View and set corresponding bindings

---
+++ GitExtensionsCourseApp/ViewModels/PersonListViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.ObjectModel;
+using GitExtensionsCourseApp.Models;
+using GitExtensionsCourseApp.Services;
+using PropertyChanged;
+
+namespace GitExtensionsCourseApp.ViewModels {
+    [AddINotifyPropertyChangedInterface]
+    public class PersonListViewModel {
+        private ITextRepository _repo;
+        public ObservableCollection<Person> PersonList { get; private set; }
+
+        public PersonListViewModel(ITextRepository repo) {
+            _repo = repo;
+            PersonList = new ObservableCollection<Person>(_repo.GetAllPersons());
+        }
+    }
+}
