c9f2d0a6397aaefc37f7e780636f21217e82b276
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-01-21 17:55:32 +0200

    Add unit tests for ActionsViewModel

---
+++ GitExtensionsCourseAppTests/ViewModels/ActionsViewModelTests.cs
@@ -0,0 +1,31 @@
+﻿using GitExtensionsCourseApp.Containers;
+using GitExtensionsCourseApp.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Unity;
+
+namespace GitExtensionsCourseAppTests.ViewModels {
+    [TestClass]
+    public class ActionsViewModelTests {
+        private ActionsViewModel _model;
+
+        [TestInitialize]
+        public void Initialize() {
+            _model = ContainerHelper.Container.Resolve<ActionsViewModel>();
+        }
+
+        [TestMethod]
+        public void TestActionsViewModel() {
+            Assert.IsNotNull(_model.Average);
+            Assert.IsFalse(_model.IsCalculated);
+        }
+
+        [TestMethod]
+        public void TestCalculateCommand() {
+            _model.CalculateCommand.Execute(null);
+
+            Assert.IsNotNull(_model.Average);
+            Assert.IsTrue(_model.IsCalculated);
+        }
+
+    }
+}
